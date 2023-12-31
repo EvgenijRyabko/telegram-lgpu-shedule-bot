78371400f11d570762dff7b1036c88415c9f9aed
EvgenijRyabko <jeniaryabko@bk.ru>
2023-12-31 13:02:52 +0300

    newCommands

--- bot/src/index.js
+++ bot/src/index.js
@@ -1,15 +1,27 @@
-import { Telegraf, Telegram } from "telegraf";
-import { message } from "telegraf/filters";
+import { Input, Telegraf } from "telegraf";
 import { logger } from "./utils/logger.js";
 import { sendSheduleJob } from "./cron/sendSheduleJob.js";
 import 'dotenv/config.js'
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-const telegram = new Telegram(process.env.BOT_TOKEN);
 
 bot.launch().catch((e) => {
   logger.error(e, "Bot");
 });
+
+bot.command("getLog", async (ctx) => {
+	if (ctx.update.message.chat.id === Number(process.env.TG_COMMAND_GROUP_ID)) {
+	  await ctx.sendDocument(Input.fromLocalFile('combined.log'));
+	  await logger.log("Sended log", "Bot");
+	}
+ });
+
+ bot.command("getErrors", async (ctx) => {
+	if (ctx.update.message.chat.id === Number(process.env.TG_COMMAND_GROUP_ID)) {
+	  await ctx.sendDocument(Input.fromLocalFile('error.log'));
+	  await logger.log("Sended error log", "Bot");
+	}
+ });
 
 bot.command("start", async (ctx) => {
   if (ctx.update.message.chat.id === Number(process.env.TG_COMMAND_GROUP_ID)) {
@@ -24,6 +36,13 @@ bot.command("stop", async (ctx) => {
     await logger.log("Job stopped", "Cron");
   }
 });
+
+bot.command("status", async (ctx) => {
+	if (ctx.update.message.chat.id === Number(process.env.TG_COMMAND_GROUP_ID)) {
+		await ctx.sendMessage(`${sendSheduleJob.running?'Cron is now running':'Cron is stopped'}`);
+		await logger.log(`Checked status`, "Cron");
+	}
+ });
 
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"));
